3ce84ca5168278b4a46fb710647e36dcad9737e9
Felipe Luis <felipeluis.6@gmail.com>
2023-05-23 19:09:43 -0300

    fix: deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -55,7 +55,6 @@ const directiveResolvers = {
     GraduationModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      typePaths: ['./**/*.graphql'],
       directiveResolvers,
       context: async ({ req, res, connection }) => {
         if (connection) {
