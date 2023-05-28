575111efec1a24b6d8ee6b171103c642ea8dbf08
Felipe Luis <felipeluis.6@gmail.com>
2023-05-28 14:21:18 -0300

    fix: deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,20 @@ import { configuration } from './config';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UseFilters } from '@nestjs/common';
 import { HttpExceptionFilter } from './http-exception.filter';
+import { join } from 'path';
 import { PrismaService } from './prisma.service';
 import { UserResolver } from './user/user.resolver';
+const schema =
+  process.env.NODE_ENV === 'development'
+    ? join(process.cwd(), 'src/schema.gql')
+    : '/tmp/src/schema.gql';
 
 @UseFilters(new HttpExceptionFilter())
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: 'src/schema.gql',
+      autoSchemaFile: schema,
       buildSchemaOptions: { dateScalarMode: 'timestamp' },
     }),
     ConfigModule.forRoot({
